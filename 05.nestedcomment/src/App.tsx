cfe9d7b4cd8573ed8bec4abe072a411df7ac6c63
Vikash Gupta <heyimvikash@gmail.com>
2025-05-08 13:38:23 +0530

    chips and nested comment

--- 05.nestedcomment/src/App.tsx
+++ 05.nestedcomment/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import {data} from "./utils/data"
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <div className="main">
